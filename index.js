3ec91b5fbaf3431125308c72b77f0a26951ebef7
Aung Zan Baw <aungzanbaw@gmail.com>
2023-02-11 18:03:30 +0900

    Test EKS

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
 // Create an AWS resource (S3 Bucket)
-const buckets = ["bucket-one", "bucket-two", "bucket-three"];
-const export_buckets = [];
+// const buckets = ["bucket-one", "bucket-two", "bucket-three"];
+// const export_buckets = [];
 
-buckets.forEach(b => export_buckets.push(new aws.s3.Bucket(b)));
+// buckets.forEach(b => export_buckets.push(new aws.s3.Bucket(b)));
 
 // Export the name of the bucket
-exports.bucketName = export_buckets;
+// exports.bucketName = export_buckets;
